16ab62d44bbeba85a2ceab6bd07f4810c0333d24
KCKluTzY <128617444+KCKluTzY@users.noreply.github.com>
2023-12-26 20:08:05 +0100

    Delete TypeParcelle.java

--- TypeParcelle.java
+++
@@ -1,9 +0,0 @@
-package modele;
-
-/**
- * type de parcelle (terrain, Arbre, sortie,Arbres2 ou Tresor)
- * @author emmanueladam Morneau Théo Molin Quentin Pilot Lucas
- */
-public enum TypeParcelle {Terrain,Arbre,Sortie,Tresor,Arbre2;
-}
-

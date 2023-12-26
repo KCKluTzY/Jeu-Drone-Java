29c674c7434e67e7047a96c0c4dd82adc706e180
KCKluTzY <128617444+KCKluTzY@users.noreply.github.com>
2023-12-26 20:08:45 +0100

    Add files via upload

---
+++ modele/TypeParcelle.java
@@ -0,0 +1,9 @@
+package modele;
+
+/**
+ * type de parcelle (terrain, Arbre, sortie,Arbres2 ou Tresor)
+ * @author emmanueladam Morneau Théo Molin Quentin Pilot Lucas
+ */
+public enum TypeParcelle {Terrain,Arbre,Sortie,Tresor,Arbre2;
+}
+
